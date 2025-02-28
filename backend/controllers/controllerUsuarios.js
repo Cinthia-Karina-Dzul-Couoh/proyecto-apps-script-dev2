9fa0170af4fc1d616ea5c1c3edca21c5f6b2680e
Karina Dzul <karin@Karina.serv.com>
2025-02-27 18:41:47 -0600

    commit 5

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -1,22 +1,36 @@
-function guardarUsuario(usuario){
+function guardarUsuario(usuario) {
     try {
-        const { id, nombreCompleto, correo, contraseña}= usuario;
         const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIOS);
-        sheetUsuarios.appendRow([id, nombreCompleto, correo, contraseña]);
-
-        return{
-            titulo: "Registro exitoso",
-            descripcion:"Ya se encuentra el usuario en la base de datos."
+        Insert(JSON.parse(usuario), sheetUsuarios);
+        return {
+        titulo: "Registro exitoso",
+        descripcion: "Ya se encuentra el usuario en la base de datos.",
         };
     } catch (error) {
-        return{
-            titulo: "Ops ha ocurrido un error!" + error,
-            descripcion:"Por favor contacte a soporte."
+        return {
+        titulo: "Ops ha ocurrido un error! " + error,
+        descripcion: "Por favor contacte a soporte.",
         };
     }
-}
+    }
 
-function listarUsuarios(id = undefined){
-    //return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
+    function listarUsuarios(id = undefined) {
+    // return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
     return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIOS), id));
+    }
+
+    function actualizarUsuario(id, datos) {
+    try {
+        const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIOS);
+        Update(id, datos, sheetUsuarios);
+        return {
+        titulo: "Actualizado correctamente",
+        descripcion: "Ya se encuentra el usuario actualizado en la base de datos.",
+        };
+    } catch (error) {
+        return {
+        titulo: "Ops ha ocurrido un error! " + error,
+        descripcion: "Por favor contacte a soporte.",
+        };
+    }
     }
